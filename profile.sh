5c4c34e3ee000bc99365351af7831d1ea292ce51
Saad Mir <saadmir@gmail.com>
2015-06-04 15:50:41 -0700

    PS1 path fix

--- profile.sh
+++ profile.sh
@@ -3,7 +3,9 @@
 alias ls='ls --color=auto'
 alias ttmux='tmux new-session -A -s saad'
 
-if [ -f "PS1.sh" ] ; then
-  . "PS1.sh"
+if [ -d "$HOME/dotfiles" ] ; then
+  if [ -f "$HOME/dotfiles/PS1.sh" ] ; then
+    . "$HOME/dotfiles/PS1.sh"
+  fi
 fi
 
