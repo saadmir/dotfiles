f96685172dbf0b950a974b36e3b970525008ca5e
Saad Mir <samir@paypal.com>
2015-07-01 16:37:37 -0700

    added ssh key copy alias

--- profile.sh
+++ profile.sh
@@ -9,8 +9,9 @@ case $OSTYPE in
     ;;
 esac
 
+alias push_ssh_key='function _push_ssh_key(){ cat ~/.ssh/id_rsa.pub | ssh $@ "mkdir ~/.ssh; cat >> ~/.ssh/authorized_keys"; }; _push_ssh_key'
 
-alias ttmux='tmux new-session -A -s saad'
+alias ttmux='tmux new-session -A -s saad';
 
 if [ -d "$HOME/dotfiles" ] ; then
   if [ -f "$HOME/dotfiles/PS1.sh" ] ; then
