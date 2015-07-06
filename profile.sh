d3eb9b104c07e6c3445d21ca56c14f030f1ee9ef
Saad Mir <samir@paypal.com>
2015-07-06 06:08:19 -0700

    path export for local/bin, t_ut for background color expansion

--- profile.sh
+++ profile.sh
@@ -11,11 +11,15 @@ esac
 
 alias push_ssh_key='function _push_ssh_key(){ cat ~/.ssh/id_rsa.pub | ssh $@ "mkdir ~/.ssh; cat >> ~/.ssh/authorized_keys"; }; _push_ssh_key'
 
-alias ttmux='tmux new-session -A -s saad';
+alias ttmux='tmux -2 new-session -A -s saad';
 
 if [ -d "$HOME/dotfiles" ] ; then
   if [ -f "$HOME/dotfiles/PS1.sh" ] ; then
     . "$HOME/dotfiles/PS1.sh"
   fi
+fi
+
+if [ -d "$HOME/local" ] && [ -d "$HOME/local/bin" ] && [[ ":$PATH:" != *":$HOME/local/bin:"* ]] ; then
+	export PATH=$HOME/local/bin:$PATH
 fi
 
