0fbbf31dee130c3abef8f436181407a8bedd88e0
Saad Mir <saadmir@gmail.com>
2025-09-15 00:51:06 -0700

    vim plugin install

--- init.sh
+++ init.sh
@@ -3,8 +3,8 @@
 cd $HOME
 
 sudo apt update
-sudo apt install build-essential cmake python3-dev nodejs npm ripgrep silversearcher-ag fzf
-sudo apt install tmux vim zellij 
+sudo apt install build-essential cmake python3-dev ripgrep silversearcher-ag fzf
+sudo apt install tmux vim 
 
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
